a9964b7fb0b9c2610fa7f3381e6e30af71434074
Cam1409 <eocanaba14@ucvvirtual.edu.pe>
2024-05-02 17:22:42 -0500

    cambios-backend

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+//import { CorsModule } from '@nestjs/common';
+
+/*async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();*/
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS para todos los dominios
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
